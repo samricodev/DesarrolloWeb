dee5acba7b6bf0ad35b96d70238b14e28b590f08
samricodev <sricoe6.8@dgb.email>
2022-11-09 23:20:33 -0600

    register products logic added

--- register_products.php
+++ register_products.php
@@ -5,13 +5,16 @@ include 'main.php';
 $nombre = $_POST['nombre'];
 $contenido = $_POST['contenido'];
 $precio = $_POST['precio'];
+$descripcion = $_POST['descripcion'];
+$imagen = $_POST['imagen'];
 
-$insertion_products = "INSERT INTO products (id, name, price, content) VALUES ('0','$nombre','$precio','$contenido')";
+$insertion_products = "INSERT INTO products (id, name, price, content, description, image) VALUES ('0','$nombre','$precio','$contenido','$descripcion','$imagen')";
 
 $set_insertion_products = mysqli_query($connect,$insertion_products);
 
 if($set_insertion_products){
     echo '<h3>Producto registrado!</h3>';
+    header('location: register_products.html');
 } else{
     echo '<h3>Registro de producto denegado</h3>';
 }
